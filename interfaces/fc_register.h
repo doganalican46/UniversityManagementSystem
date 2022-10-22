3552f3795cc1349b6754bfe22e4c18e7afc9e93d
Ali Can Dogan <76850300+alleycan@users.noreply.github.com>
2022-10-22 14:27:11 +0300

    Interfaces added

---
+++ interfaces/fc_register.h
@@ -0,0 +1,175 @@
+#include "fc_register.h"
+
+#pragma once
+
+namespace universitymanagementsystem {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for fc_register
+	/// </summary>
+	public ref class fc_register : public System::Windows::Forms::Form
+	{
+	public:
+		fc_register(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~fc_register()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ updatebutton;
+	protected:
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(fc_register::typeid));
+			this->updatebutton = (gcnew System::Windows::Forms::Button());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// updatebutton
+			// 
+			this->updatebutton->BackColor = System::Drawing::Color::MidnightBlue;
+			this->updatebutton->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(162)));
+			this->updatebutton->ForeColor = System::Drawing::Color::White;
+			this->updatebutton->Location = System::Drawing::Point(21, 215);
+			this->updatebutton->Name = L"updatebutton";
+			this->updatebutton->Size = System::Drawing::Size(120, 59);
+			this->updatebutton->TabIndex = 51;
+			this->updatebutton->Text = L"ADD STUDENT";
+			this->updatebutton->UseVisualStyleBackColor = false;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Times New Roman", 8, System::Drawing::FontStyle::Bold));
+			this->label6->Location = System::Drawing::Point(181, 144);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(237, 13);
+			this->label6->TabIndex = 45;
+			this->label6->Text = L"FACULTY COORDINATOR REGISTER PAGE";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(162)));
+			this->label4->Location = System::Drawing::Point(203, 59);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(179, 15);
+			this->label4->TabIndex = 44;
+			this->label4->Text = L"University Management System\r\n";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(162)));
+			this->label5->Location = System::Drawing::Point(156, 40);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(275, 19);
+			this->label5->TabIndex = 43;
+			this->label5->Text = L"EUROPEAN UNIVERSITY OF LEFKE";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::Color::Transparent;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(0, 7);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(150, 150);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 42;
+			this->pictureBox1->TabStop = false;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::MidnightBlue;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(162)));
+			this->button1->ForeColor = System::Drawing::Color::White;
+			this->button1->Location = System::Drawing::Point(160, 215);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(120, 59);
+			this->button1->TabIndex = 52;
+			this->button1->Text = L"ADD INSTRUCTOR";
+			this->button1->UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::MidnightBlue;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(162)));
+			this->button2->ForeColor = System::Drawing::Color::White;
+			this->button2->Location = System::Drawing::Point(298, 215);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(120, 59);
+			this->button2->TabIndex = 53;
+			this->button2->Text = L"ADD COURSE";
+			this->button2->UseVisualStyleBackColor = false;
+			// 
+			// fc_register
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(430, 402);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->updatebutton);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"fc_register";
+			this->Text = L"fc_register";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
